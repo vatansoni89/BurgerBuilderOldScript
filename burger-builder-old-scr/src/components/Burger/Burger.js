be7d36d8dc9da87ad769fd5baffb7679637289d0
Vatan Soni <vatansoni89@gmail.com>
2019-10-07 17:42:10 +0530

    reduce(): check if no ingredients added.

--- burger-builder-old-scr/src/components/Burger/Burger.js
+++ burger-builder-old-scr/src/components/Burger/Burger.js
@@ -2,14 +2,22 @@ import React from "react";
 import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    // console.log(igKey, props.ingredients[igKey], [
-    //   ...Array(props.ingredients[igKey])
-    // ]);
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      // console.log(igKey, props.ingredients[igKey], [
+      //   ...Array(props.ingredients[igKey])
+      // ]);
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      //without this it's array of array.
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients.</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />

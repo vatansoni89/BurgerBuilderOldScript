ea40ed6d8b0109b725ed5d724572996fb1cc2b71
Vatan Soni <vatansoni89@gmail.com>
2019-10-07 13:52:57 +0530

    Started Layout and BurgerBuilder

--- burger-builder-old-scr/src/App.js
+++ burger-builder-old-scr/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout from "./components/Layout/Layout";
 
 class App extends Component {
@@ -6,7 +7,7 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
--- burger-builder-old-scr/src/components/Layout/Layout.js
+++ burger-builder-old-scr/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import Aux from "../../hoc/Auxiliary";
 import React from "react";
+import classes from "./Layout.css";
 
 const layout = props => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <div>{props.children}</div>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 

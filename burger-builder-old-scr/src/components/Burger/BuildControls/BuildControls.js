46a0460146bce64fd15af56428d59a5308c21035
Vatan Soni <vatansoni89@gmail.com>
2019-10-09 18:25:47 +0530

    Displaying and Updating Burger Price.

--- burger-builder-old-scr/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder-old-scr/src/components/Burger/BuildControls/BuildControls.js
@@ -11,6 +11,9 @@ const controls = [
 
 const buildControls = props => (
   <div className={classes.BuildControls}>
+    <div>
+      Total Price: <strong>{props.price.toFixed(2)}</strong>
+    </div>
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
